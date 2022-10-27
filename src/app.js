24d6861ba4a02c75021d8f1dc1dfcc01e9053526
Matiaslopezp <lopezp.matias@gmail.com>
2022-10-27 20:54:40 +0000

    mi primer cambio en el archivo

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  var dom = [".com", ".cl"];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let w = 0; w < noun.length; w++) {
+        for (let h = 0; h < dom.length; h++) {
+          console.log(pronoun[i] + adj[j] + noun[w] + dom[h]);
+        }
+      }
+    }
+  }
 };
